50a368b39cb33466e5a08834f6dda46ea874662b
Phinjock Sherpa <phinjock@gmail.com>
2023-04-13 16:44:55 -0700

    working on displaying X & O'

--- src/App.js
+++ src/App.js
@@ -2,29 +2,35 @@ import React, { useState } from "react";
 import "./index.css";
 
 export default function App() {
-  const [player, setPlayer] = useState("X");
+  const [player, setPlayer] = useState(null);
 
-  // const handleClick=(e)=>setPlayer(e.target.value)
+  const renderSqure = () => {
+    return <div>'X'</div>;
+  };
 
-  // }
+  const handleClick = (buttonId) => {
+    setPlayer(buttonId);
+  };
 
   return (
     <div className="App">
       <h1>tic tac toe</h1>
       <div className="square">
-        <button></button>
-        <button></button>
-        <button></button>
+        <button className="clicked" onClick={() => handleClick(1)}>
+          {player}
+        </button>
+        <button onClick={() => handleClick(2)}>{player}</button>
+        <button key={2}></button>
       </div>
       <div className="square">
-        <button></button>
-        <button></button>
-        <button></button>
+        <button key={3}></button>
+        <button key={4}></button>
+        <button key={5}></button>
       </div>
       <div className="square">
-        <button></button>
-        <button></button>
-        <button></button>
+        <button key={6}></button>
+        <button key={7}></button>
+        <button key={8}></button>
       </div>
     </div>
   );
