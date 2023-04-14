ac2eeec9d5dfe22a2d25e030b91c16360a51ed7a
Phinjock Sherpa <phinjock@gmail.com>
2023-04-14 11:35:31 -0700

    X and O are being displayed in designated position

--- src/App.js
+++ src/App.js
@@ -6,44 +6,36 @@ export default function App() {
   const [playerO, setPlayerO] = useState("O");
   const [board, setBoard] = useState(null);
   const [position, setPosition] = useState([
-    { id: 1 },
-    { id: 2 },
-    { id: 3 },
-    { id: 4 },
-    { id: 5 },
-    { id: 6 },
-    { id: 7 },
-    { id: 8 },
-    { id: 9 },
+    { id: 1, value: board },
+    { id: 2, value: board },
+    { id: 3, value: board },
+    { id: 4, value: board },
+    { id: 5, value: board },
+    { id: 6, value: board },
+    { id: 7, value: board },
+    { id: 8, value: board },
+    { id: 9, value: board },
   ]);
 
-  const handleClick = (index) => {
-    if (index) {
-      return setBoard(board === playerX ? playerO : playerX);
-    }
+  const handleClick = (id) => {
+    const updateBoard = position.map((item) => {
+      if (id === item.id) {
+        item.value = board;
+      }
+      return item;
+    });
+    setBoard(board === playerX ? playerO : playerX);
   };
 
   return (
     <div className="App">
       <h1>tic tac toe</h1>
-      <div className="square">
-        <button className="clicked" onClick={() => handleClick(0)}>
-          {board}
-        </button>
-        <button className="clicked" onClick={() => handleClick(1)}>
-          {board}
-        </button>
-        <button className="clicked"></button>
-      </div>
-      <div className="square">
-        <button className="clicked"></button>
-        <button className="clicked"></button>
-        <button className="clicked"></button>
-      </div>
-      <div className="square">
-        <button className="clicked"></button>
-        <button className="clicked"></button>
-        <button className="clicked"></button>
+      <div className="grid">
+        {position.map((item) => (
+          <button className="clicked" onClick={() => handleClick(item.id)}>
+            {item.value}
+          </button>
+        ))}
       </div>
     </div>
   );
