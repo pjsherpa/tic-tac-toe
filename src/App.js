2e0a4db1517c008ef963f8dec7063f908de0045c
Phinjock Sherpa <phinjock@gmail.com>
2023-04-14 07:59:51 -0700

    WIP using id's to specify squares

--- src/App.js
+++ src/App.js
@@ -5,19 +5,32 @@ export default function App() {
   const [playerX, setPlayerX] = useState("X");
   const [playerO, setPlayerO] = useState("O");
   const [board, setBoard] = useState(null);
+  const [position, setPosition] = useState([
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+    { id: 6 },
+    { id: 7 },
+    { id: 8 },
+    { id: 9 },
+  ]);
 
-  const handleClick = () => {
-    setBoard(board === playerX ? playerO : playerX);
+  const handleClick = (index) => {
+    if (index) {
+      return setBoard(board === playerX ? playerO : playerX);
+    }
   };
 
   return (
     <div className="App">
       <h1>tic tac toe</h1>
       <div className="square">
-        <button className="clicked" onClick={() => handleClick()}>
+        <button className="clicked" onClick={() => handleClick(0)}>
           {board}
         </button>
-        <button className="clicked" onClick={() => handleClick()}>
+        <button className="clicked" onClick={() => handleClick(1)}>
           {board}
         </button>
         <button className="clicked"></button>
