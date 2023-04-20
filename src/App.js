165e4b92eb7829cf4da0ffafad72c0fc425f86e4
Phinjock Sherpa <phinjock@gmail.com>
2023-04-20 15:46:53 -0700

    winning combination now created, WIP values need to be updated individually

--- src/App.js
+++ src/App.js
@@ -2,42 +2,69 @@ import React, { useState } from "react";
 import "./index.css";
 
 export default function App() {
-  const [playerX, setPlayerX] = useState("X");
-  const [playerO, setPlayerO] = useState("O");
-  const [isDisabled, setDisabled] = useState(false);
-  const [board, setBoard] = useState("");
+  // const [status, setStatus] = useState("");
+  const [board, setBoard] = useState("X");
   const [position, setPosition] = useState([
-    { id: 1, value: board },
-    { id: 2, value: board },
-    { id: 3, value: board },
-    { id: 4, value: board },
-    { id: 5, value: board },
-    { id: 6, value: board },
-    { id: 7, value: board },
-    { id: 8, value: board },
-    { id: 9, value: board },
+    { id: 1, value: "" },
+    { id: 2, value: "" },
+    { id: 3, value: "" },
+    { id: 4, value: "" },
+    { id: 5, value: "" },
+    { id: 6, value: "" },
+    { id: 7, value: "" },
+    { id: 8, value: "" },
+    { id: 9, value: "" },
   ]);
 
   const startGame = (id) => {
-    position.map((item) => {
-      if (id === item.id) {
+    const updatePosition = position.map((item) => {
+      if (item.id === id && item.value === "") {
         item.value = board;
       }
-      setBoard(board === playerX ? playerO : playerX);
+      setBoard(board === "X" ? "O" : "X");
+      return item;
     });
-    // setDisabled(true);
+    setPosition(updatePosition);
+  };
+
+  //Connect idx eg:1,2,3 from a different object within an array to create logic
+  //winpoistion not being triggered
+
+  const winPosition = (a) => {
+    position.map((item) => {
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+      for (let i = 0; i < lines.length; i++) {
+        let [a, b, c] = lines[i];
+        if (
+          (a = item.value) &&
+          (a = item.value) === (b = item.value) &&
+          (a = item.value) === (c = item.value)
+        ) {
+          // return <h2>{`winning number ${item.value[a]}`}</h2>;
+          console.log("Winner", [a, b, c], [a]);
+        }
+      }
+      return null;
+    });
   };
 
   return (
     <div className="App">
       <h1 className="gameOn">Tic-Tac-Toe</h1>
+      {/* {status} */}
+      {winPosition()}
       <div className="grid">
         {position.map((item) => (
-          <button
-            disabled={isDisabled}
-            className="clicked"
-            onClick={() => startGame(item.id)}
-          >
+          <button className="clicked" onClick={() => startGame(item.id)}>
             {item.value}
           </button>
         ))}
